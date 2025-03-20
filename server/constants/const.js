067d6cbc4df68069169116f983628a0f3ee2c511
amamdik <amamdik@gmail.com>
2025-03-20 21:38:13 +0530

    initial commit

---
+++ server/constants/const.js
@@ -0,0 +1,79 @@
+const productsList = [
+    {
+        "id": 1,
+        "name": "Margherita Pizza ",
+        "price": "3500",
+        "currency": "LKR",
+        "description": "Classic pizza with tomato, mozzarella, and basil..",
+        "image": "margherita.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Pepper Salami pizza",
+        "price": "4500",
+        "currency": "LKR",
+        "description": "Cheesy delight topped with spicy pepperoni slices..",
+        "image": "pepperoni.jpg"
+    },
+    {
+        "id": 3,
+        "name": "PedroTech Special Pizza ",
+        "price": "2500",
+        "currency": "LKR",
+        "description": "Delicious pizza with unique, flavorful ingredient combinations..",
+        "image": "pedrotechspecial.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Vegan Pizza",
+        "price": "3000",
+        "currency": "LKR",
+        "description": "Delicious plant-based pizza, cruelty-free and healthy..",
+        "image": "vegan.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Pineapple Pizza",
+        "price": "2000",
+        "currency": "LKR",
+        "description": "Tropical pineapple meets savory pizza in delicious harmony..",
+        "image": "pineapple.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Very Expensive Pizza",
+        "price": "4000",
+        "currency": "LKR",
+        "description": "Luxury pizzas with premium ingredients, priced extravagantly..",
+        "image": "expensive.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Mediterranean Feast Pizza",
+        "price": "3200",
+        "currency": "LKR",
+        "description": "Artichoke, olives, sun-dried tomatoes, feta, arugula, balsamic glaze..",
+        "image": "Mediterranean Feast Pizza.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Supreme Delight Pizza",
+        "price": "3800",
+        "currency": "LKR",
+        "description": "Indulge in the ultimate pizza experience with a perfect blend of premium meats, fresh vegetables, and a hint of spice for an unforgettable flavor explosion..",
+        "image": "Supreme Delight Pizza.jpeg"
+    },
+    {
+        "id": 9,
+        "name": "Supreme Delight Pizza",
+        "price": "3500",
+        "currency": "LKR",
+        "description": "Tangy BBQ sauce, grilled chicken, red onions, cilantro..",
+        "image": "bbqpizza.webp"
+    }
+
+]
+
+const test = [];
+
+module.exports = {productsList,test};
